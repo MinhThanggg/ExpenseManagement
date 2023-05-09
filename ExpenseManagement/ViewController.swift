84ffa48890a1b7ca8bbbdeb9ddff2e87b0a77fdf
MinhThang <20211tt1477@mail.tdc.edu.vn>
2023-05-09 14:07:19 +0700

    Initial Commit

---
+++ ExpenseManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpenseManagement
+//
+//  Created by CNTT on 5/9/23.
+//  Copyright © 2023 fit.tdc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
