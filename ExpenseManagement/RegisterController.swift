169fdca0d0c79e460f12a6692ecb1dd46a513bb6
MinhThang <20211tt1477@mail.tdc.edu.vn>
2023-05-23 22:56:13 +0700

    up UI

---
+++ ExpenseManagement/RegisterController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterController.swift
+//  ExpenseManagement
+//
+//  Created by CNTT on 5/23/23.
+//  Copyright © 2023 fit.tdc. All rights reserved.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
