d3fc412138df1f5ac73b1f2d60a66910fda2340b
MinhThang <20211tt1477@mail.tdc.edu.vn>
2023-05-30 15:17:11 +0700

    up date

--- ExpenseManagement/CategoryDetailController.swift
+++ ExpenseManagement/CategoryDetailController.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
-class CategoryDetailController: UIViewController {
+class CategoryDetailController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var category:Category?
 
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var imgCategory: UIImageView!
+    @IBOutlet weak var btnSave: UIBarButtonItem!
+    
+    @IBAction func imageProccessing(_ sender: UITapGestureRecognizer) {
+        print("chuyen man hinh")
+        let image = UIImagePickerController()
+        image.sourceType = .photoLibrary
+        image.delegate = self
+        present(image, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imageSelected = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imgCategory.image = imageSelected
+        }
+        // Quay ve man hinh truoc do
+        dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func btnCancel(_ sender: UIBarButtonItem) {
           dismiss(animated: true, completion: nil)
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if let button = sender as? UIBarButtonItem, button === btnSave {
+            let name = txtName.text ?? ""
+            let img = imgCategory.image
+            category = Category(name: name, image: img)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
